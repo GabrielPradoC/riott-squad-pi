619e985a3e6de677f6f081ac9cdefa55ad5bfeec
Vinícius Seabra <vseabra@protonmail.com>
2021-11-26 10:41:52 -0300

    docs: explicação do middleware de autenticação.

--- back/src/modules/authentication/v1/middlewares/passport.ts
+++ back/src/modules/authentication/v1/middlewares/passport.ts
@@ -16,19 +16,31 @@ passport.use(
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey: jwtSecret
         },
-        // disponibiliza o usuário no objeto request (req.user)
+        /**
+         * estrtégia de autenticação.
+         *
+         * a função desse middleware é disponiblizar uma referencia a entidade do usuario no request.
+         * Se o usuario existir no banco de dados, ele vai poder ser acessado por req.user no controller
+         * @param decodedToken - O jwt decodificado.
+         */
         async (decodedToken: any, done: VerifiedCallback) => {
             const userRepository: UserRepository = new UserRepository();
             const user: User | undefined = await userRepository.findOne(decodedToken.id);
             if (user) {
                 done(null, user);
             } else {
-                done(null, false, { message: 'usuário não encontrado' });
+                done(null, false, { error: 'usuário não encontrado' });
             }
         }
     )
 );
 
+/**
+ * authMiddleware.
+ * retorna o middleware de autenticação
+ *
+ * @returns o middleware de autenticação.
+ */
 export const authMiddleware = (): RequestHandler => {
     return passport.authenticate('jwt', jwtSession);
 };
