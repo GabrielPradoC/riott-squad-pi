098bd778140d05cefcec143a7a71676e2c7c4efe
Letícia Genes <leticia_genes@hotmail.com>
2021-12-19 19:11:55 -0400

    feat: funcionalidade input date do cadastro

--- front/src/app/pages/members/members.component.ts
+++ front/src/app/pages/members/members.component.ts
@@ -7,8 +7,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./members.component.scss']
 })
 export class MembersComponent {
-
-
   private form: FormGroup;
   static fileTemp: File;
 
@@ -27,20 +25,35 @@ export class MembersComponent {
         Validators.required
       ])]
     });
+  }
 
+  ngOnInit() {
     window.onload = function () {
-      document.getElementById("uploadFoto").ondragover = (event) => { event.preventDefault(); }
-      document.getElementById("uploadFoto").ondrop = (event) => {
-        event.preventDefault();
-        const files = event.dataTransfer.files;
-    
-        if(files.length > 1) {
-          alert("É permitido o envio de apenas uma imagem");
-        } else {
-          MembersComponent.prototype.setFileTemp(files.item(0));
-        }
+      MembersComponent.initAttributesDrop();
+      MembersComponent.initAttributesDate();
+    }
+  }
+
+  static initAttributesDrop() : void {
+    document.getElementById("uploadFoto").ondragover = (event) => { event.preventDefault(); }
+    document.getElementById("uploadFoto").ondrop = (event) => {
+      event.preventDefault();
+      const files = event.dataTransfer.files;
+  
+      if(files.length > 1) {
+        alert("É permitido o envio de apenas uma imagem");
+      } else {
+        MembersComponent.prototype.setFileTemp(files.item(0));
       }
     }
+  }
+
+  static initAttributesDate() : void {
+    const today: Date = new Date();
+    const month: string = "-" + (today.getMonth() + 1) + "-";
+
+    document.getElementById("dataNascimento").setAttribute("min", (today.getFullYear() - 18) + month + (today.getDate()+1));
+    document.getElementById("dataNascimento").setAttribute("max", today.getFullYear() + month + (today.getDate()-1));
   }
 
   setFileTemp(file: File) : void {
@@ -120,6 +133,43 @@ export class MembersComponent {
   labelUp() {
     document.getElementById("dateLabel").style.transform = "translateY(-22px)";
     document.getElementById("dateLabel").style.color = "#7C8D93";
+  }
+
+  onChange() {
+    const birthday = (<HTMLSelectElement>document.getElementById("dataNascimento")).value;
+    if(birthday) {
+      const year = parseInt(birthday.substring(0, 4));
+      const month = parseInt(birthday.substring(5, 7));
+      const day = parseInt(birthday.substring(8, 10));
+      const today = new Date();
+
+      if((year > (today.getFullYear()-18)) && (year < today.getFullYear())) {
+        return;
+      } else if(year === (today.getFullYear()-18)) {
+        if(month > (today.getMonth()+1)) {
+          return;
+        } else if(month === (today.getMonth()+1)) {
+          if(day > today.getDate()) {
+            return;
+          }
+        }
+      } else if(year === today.getFullYear()) {
+        if(month < (today.getMonth()+1)) {
+          return;
+        } else if(month === (today.getMonth()+1)) {
+          if(day < today.getDate()) {
+            return;
+          }
+        }
+      }
+
+      if(year >= today.getFullYear()) {
+        alert("Data inválida, insira uma data menor que a atual")
+      } else {
+        alert("O membro deve ser menor de 18 anos")
+      }
+      this.form.controls['dataNascimento'].setErrors({ required: true });
+    }
   }
 
   cadastrarMembro() : void {
