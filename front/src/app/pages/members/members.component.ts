7d236af6b5084cb795db97d9400eaf3cdc8bf26f
Letícia Genes <leticia_genes@hotmail.com>
2021-12-19 13:35:17 -0400

    feat: funcionalidade input file do cadastro (finalizada)

--- front/src/app/pages/members/members.component.ts
+++ front/src/app/pages/members/members.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,7 +7,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./members.component.scss']
 })
 export class MembersComponent {
-  public form: FormGroup;
+
+
+  private form: FormGroup;
+  static fileTemp: File;
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
@@ -24,37 +27,69 @@ export class MembersComponent {
         Validators.required
       ])]
     });
+
+    window.onload = function () {
+      document.getElementById("uploadFoto").ondragover = (event) => { event.preventDefault(); }
+      document.getElementById("uploadFoto").ondrop = (event) => {
+        event.preventDefault();
+        const files = event.dataTransfer.files;
+    
+        if(files.length > 1) {
+          alert("É permitido o envio de apenas uma imagem");
+        } else {
+          MembersComponent.prototype.setFileTemp(files.item(0));
+        }
+      }
+    }
+  }
+
+  setFileTemp(file: File) : void {
+    MembersComponent.fileTemp = file;
+  }
+
+  getFileDrop() {
+    MembersComponent.fileTemp = null;
+    
+    setTimeout(() => { this.checkFile(MembersComponent.fileTemp); }, 100);
   }
 
   fileChangeEvent(inputFile: any) : void {
-    const file: File = inputFile.target.files[0];
+    this.checkFile(inputFile.target.files[0]);
+  }
+
+  async checkFile(file: File) {
     let error: string;
 
     if(!file) {
       error = "Houve um problema no upload da imagem, tente novamente";
     } else {
-      if (!(file.type === 'image/png' || 'image/jpeg')) {
+      if (!(file.type === 'image/png' || file.type === 'image/jpeg')) {
         error = "Apenas são permitidas imagens nos formatos PNG e JPG";
       } else {
         if(file.size > 10485760) {
           error = "O tamanho da imagem não deve ultrapassar 10MB";
         } else {
-          if(!this.saveImage(file)) {
+          if(!(await this.saveImage(file))) {
             error = "Não foi possível abrir a imagem, tente novamente";
           } else {
+            this.form.controls['foto'].setErrors(null);
+
             return;
           }
         }
       }
     }
 
-    this.form.controls['foto'].setErrors({ required: false });
-    console.log(error);
+    document.getElementById("uploadFoto").firstElementChild.firstElementChild.setAttribute("class", "");
+    document.getElementById("uploadFoto").firstElementChild.firstElementChild.setAttribute("src", "../../../assets/file.ico");
+    this.form.controls['foto'].setErrors({ required: true });
+    alert(error);
   }
 
-  saveImage(image: File) : boolean {
+  async saveImage(image: File) : Promise<boolean> {
     const reader: FileReader = new FileReader();
     let imageTemp: string;
+    localStorage.removeItem("RIOTT:imgTemp");
 
     reader.onload = (e: any) => {
       localStorage.setItem("RIOTT:imgTemp", e.target.result);
@@ -62,54 +97,44 @@ export class MembersComponent {
 
     reader.readAsDataURL(image);
 
+    await this.delay(100);
+  
     imageTemp = localStorage.getItem("RIOTT:imgTemp");
 
     if(imageTemp) {
-      //bota a img pra mostrar no form
-      document.getElementById("uploadFoto").firstElementChild.setAttribute("src", imageTemp);
+      document.getElementById("uploadFoto").firstElementChild.firstElementChild.setAttribute("class", "imgUpload");
+      document.getElementById("uploadFoto").firstElementChild.firstElementChild.setAttribute("src", imageTemp);
       return true;
     }
     return false;
   }
 
+  delay(ms: number): Promise<boolean> {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(true);
+      }, ms);
+    });
+  }
 
-
-
-
+  labelUp() {
+    document.getElementById("dateLabel").style.transform = "translateY(-22px)";
+    document.getElementById("dateLabel").style.color = "#7C8D93";
+  }
 
   cadastrarMembro() : void {
+    const foto = localStorage.getItem("RIOTT:imgTemp");
     const nome: string = this.form.controls['nome'].value;
     const dataNascimento = this.form.controls['dataNascimento'].value;
     const valorMesada = this.form.controls['valorMesada'].value;
 
 
-    console.log();
+    console.log(foto);
     console.log(nome);
     console.log(dataNascimento);
     console.log(valorMesada);
 
     
   }
-
-  onDrop(event: any) {
-    event.preventDefault();
-    event.stopPropagation();
-    // your code goes here after droping files or any
-    }
-
-    onDragOver(evt) {
-      console.log("Drag Over");
-      return false;
-    }
-
-   onDragLeave(evt) {
-     evt.preventDefault();
-     evt.stopPropagation();
-    }
-
-    labelUp() {
-      document.getElementById("dateLabel").style.transform = "translateY(-22px)";
-      document.getElementById("dateLabel").style.color = "#7C8D93";
-    }
 }
 
